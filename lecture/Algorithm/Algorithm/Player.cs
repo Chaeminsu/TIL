9e0abf0ce9a1533b119545d8398d6a536781ee0e
coalstn789 <tyoyt7@gmail.com>
2020-09-21 22:55:58 +0900

    BFS 최단 거리 길찾기 구현 다익스트라 최단거리 구현 Tree 개념 추가

--- lecture/Algorithm/Algorithm/Player.cs
+++ lecture/Algorithm/Algorithm/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,40 +40,9 @@ namespace Algorithm
             PosY = posY;
             _board = board;
 
-            //현재 바라고보 있는 방향을 기준으로 좌표 변화를 나타낸다.
-            int[] frontY = new int[] {-1, 0, 1, 0 };
-            int[] frontX = new int[] { 0, -1, 0, 1 };
-            int[] rightY = new int[] { 0, -1, 0, 1 };
-            int[] rightX = new int[] { 1, 0, -1, 0 };
+            BFS();
 
-            _points.Add(new Pos(PosY, PosX));
-            //목적지 도착전 실행
-            while (PosY != board.DestY || PosX != board.DestX)
-            {
-                //1. 현재 바라보는 방향 기준 오른쪽 진행 가능 여부 확인
-                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
-                {
-                    // 오른쪽 방향으로 90회전 
-                    _dir = (_dir - 1 + 4) % 4;
-                    // 앞으로 전진
-                    PosX = PosX + frontX[_dir];
-                    PosY = PosY + frontY[_dir];
-                    _points.Add(new Pos(PosY, PosX));
-
-                }
-                else if (_board.Tile[PosY + frontY[_dir],PosX + frontX[_dir]] == Board.TileType.Empty)
-                {
-                    // 앞으로 전진
-                    PosX = PosX + frontX[_dir];
-                    PosY = PosY + frontY[_dir];
-                    _points.Add(new Pos(PosY, PosX));
-                }
-                else  
-                {
-                    //2. 왼쪽으로 90 회전
-                    _dir = (_dir + 1 + 4) % 4;
-                }
-            }
+           
 
         }
         const int MOVE_TICK = 10;
@@ -92,6 +62,101 @@ namespace Algorithm
                 PosY = _points[_lastIndex].Y;
                 PosX = _points[_lastIndex].X;
                 _lastIndex++;
+            }
+        }
+        public void BFS()
+        {
+            int[] deltaY = new int[] { -1,0,1,0 };
+            int[] deltaX = new int[] { 0,-1,0,1 };
+
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (q.Count > 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+
+                    if (nextX < 0 || nextX > _board.Size || nextY < 0 || nextY > _board.Size)
+                        continue;
+
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                    {
+                        continue;
+                    }
+                    if (found[nextY,nextX])
+                    {
+                        continue;
+                    }
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+
+            while (parent[y,x].Y != y || parent[y,x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
+
+        }
+        public void RightHand()
+        {
+            //현재 바라고보 있는 방향을 기준으로 좌표 변화를 나타낸다.
+            int[] frontY = new int[] { -1, 0, 1, 0 };
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+            int[] rightY = new int[] { 0, -1, 0, 1 };
+            int[] rightX = new int[] { 1, 0, -1, 0 };
+
+            _points.Add(new Pos(PosY, PosX));
+            //목적지 도착전 실행
+            while (PosY != _board.DestY || PosX != _board.DestX)
+            {
+                //1. 현재 바라보는 방향 기준 오른쪽 진행 가능 여부 확인
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+                {
+                    // 오른쪽 방향으로 90회전 
+                    _dir = (_dir - 1 + 4) % 4;
+                    // 앞으로 전진 
+                    PosX = PosX + frontX[_dir];
+                    PosY = PosY + frontY[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+
+                }
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                {
+                    // 앞으로 전진
+                    PosX = PosX + frontX[_dir];
+                    PosY = PosY + frontY[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else
+                {
+                    //2. 왼쪽으로 90 회전
+                    _dir = (_dir + 1 + 4) % 4;
+                }
             }
         }
 
--- lecture/Algorithm/Exercize/Program.cs
+++ lecture/Algorithm/Exercize/Program.cs
@@ -5,15 +5,27 @@ namespace Exercize
 {
     class Graph
     {
+
         int[,] adj = new int[6, 6]
         {
-            {0,1,0,1,0,0},
-            {1,0,1,1,0,0},
-            {0,1,0,0,0,0},
-            {1,1,0,0,1,0},
-            {0,0,0,1,0,1},
-            {0,0,0,0,1,0},
+            {-1,15,-1,35,-1,-1},
+            {15,-1,05,10,-1,-1},
+            {-1,05,-1,-1,-1,-1},
+            {35,10,-1,-1,05,-1},
+            {-1,-1,-1,05,-1,05},
+            {-1,-1,-1,-1,05,-1},
         };
+
+      
+        //int[,] adj = new int[6, 6]
+        //{
+        //    {0,1,0,1,0,0},
+        //    {1,0,1,1,0,0},
+        //    {0,1,0,0,0,0},
+        //    {1,1,0,0,1,0},
+        //    {0,0,0,1,0,1},
+        //    {0,0,0,0,1,0},
+        //};
 
         List<int>[] adj2 = new List<int>[]
         {
@@ -74,7 +86,70 @@ namespace Exercize
 
         }
 
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
 
+            Array.Fill(distance, Int32.MaxValue);
+
+            distance[start] = 0;
+            parent[start] = start;
+            while (true)
+            {
+                // 제일 좋은 후보 찾기
+                // 가장 유력한 후보의 거리와 번호를 저장
+
+                int closeset = Int32.MaxValue;
+                int now = -1;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    //이미 방문한 정점은 스킵
+                    if (visited[i])
+                        continue;
+                    // 예악된 적 없거나, 기존 후보보다 멀리 있으면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closeset)
+                        continue;
+
+                    // 여태껏 발견한 가장 후보 정보 갱신
+                    closeset = distance[i];
+                    now = i;
+
+
+                }
+
+                if (now == -1)
+                    break;
+                // 제일 좋은 후보 찾음.
+                visited[now] = true;
+                // 방문한 정점과 인접한 정점들을 조사해서, 상황에 따라 발견한 최단거리를 갱신한다.
+                for (int next = 0; next < 6; next++)
+                {
+                    // 연결되지 않은 정점 스킵
+                    if (adj[now, next] == -1)
+                    {
+                        continue;
+                    }
+                    // 이미 방ㅈ문한 정점 스킵
+                    if (visited[next])
+                    {
+                        continue;
+                    }
+
+                    // 새로 조사된 정점의 최단거리를 계산한다.
+                    int nextDis = distance[now] + adj[now, next];
+
+                    //기존 발견 한 최단거리가 새로 조사된 최단거리보다 크면 정보 갱신
+                    if (nextDis < distance[next])
+                    {
+                        distance[next] = nextDis;
+                        parent[next] = now;
+                    }
+                }
+            }
+        }
         public void BFS(int start)
         {
             bool[] found = new bool[6];
@@ -111,21 +186,78 @@ namespace Exercize
         }
     }
 
+
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+
+    }
+
     class Program
     {
+        public static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    //node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    //node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    //node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    //node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                    //node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Children.Add(node);
+                }
+            }
+
+            return root;
+        }
+
+        public static void  PrintTree(TreeNode<string> root)
+        {
+
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> Child in root.Children)
+                PrintTree(Child);
+
+
+
+
+        }
+        public static int GetHeight(TreeNode<string> root)
+        {
+            int heigth = 0;
+
+            foreach (TreeNode<string> Child in root.Children)
+            {
+                int newHeight = GetHeight(Child)+1;
+                if (heigth < newHeight)
+                {
+                    heigth = newHeight;
+                }
+            }
+            return heigth;
+        }
         static void Main(string[] args)
         {
-            // DFS ( 깊이 우선 탐색 Depth First Search)
-            Graph graph = new Graph();
-            graph.BFS(0);
+            TreeNode<string> root = MakeTree();
+            PrintTree(root);
 
-
-
-            // BFS ( 너비 우선 탐색 Breadth First Search)
-
-
-
-
+            Console.WriteLine(GetHeight(root));
+           
         }
     }
 }
