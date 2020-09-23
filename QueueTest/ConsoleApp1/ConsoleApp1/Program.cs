df3c6a182a23482094da95da4cf71b0b5c829ed2
coalstn789 <tyoyt7@gmail.com>
2020-09-23 22:36:58 +0900

    Queue 관련 연습 문제 커밋

---
+++ QueueTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int[] value = new int[] { 1, 2, 3, 2, 3 };
+        static void Main(string[] args)
+        {
+            foreach (int item in solution(value))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static int[] solution(int[] prices)
+        {
+            List<int> answer = new List<int>();
+
+            Queue<int> q = new Queue<int>(prices);
+            int index = 1;
+            while (q.Count > 0)
+            {
+                int nowPrice = q.Dequeue();
+                int time = 0;
+
+                for (int i = index; i < prices.Length; i++)
+                {
+                    time++;
+                    if (nowPrice > prices[i] || i == prices.Length - 1)
+                    {
+                        answer.Add(time);
+                        break;
+                    }
+                }
+                index++;
+            }
+            answer.Add(0);
+            return answer.ToArray();
+        }
+    }
+}
