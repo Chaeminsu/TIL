8f5f5d7d773468bc878067b35e2ba43e9a735620
coalstn789 <tyoyt7@gmail.com>
2020-10-09 00:09:14 +0900

    프로그래머스 - 나누어 떨어지는 숫자배열

---
+++ Programers_Level1/Programers_Level1-7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_7
+{
+    class Program
+    {
+        static int[] array = { 8,9,7,11 };
+
+        static void Main(string[] args)
+        {
+            foreach (int item in solution(array, 5))
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        public static int[] solution(int[] arr, int divisor)
+        {
+            List<int> answer = new List<int>();
+
+            answer = arr.Where(x => x % divisor == 0).OrderBy(y => y).ToList();
+
+            if (answer.Count <= 0)
+                answer.Add(-1);
+
+            return answer.ToArray();
+        }
+    }
+}
