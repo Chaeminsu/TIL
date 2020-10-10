2559ac14e9298aeea80e52b4f485f4cdd444d793
coalstn789 <tyoyt7@gmail.com>
2020-10-10 11:15:07 +0900

    프로그래머스 - 두정수 사이의 합

---
+++ Programers_Level1/Programers_Level1-8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_8
+{
+    class Program
+    {
+        static int a = 3, b = 5;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(a,b));
+        }
+        public static long solution(int a, int b)
+        {
+            long answer = 0;
+            if (a == b)
+            {
+                answer = a;
+                return answer;
+            }
+
+            int start = 0;
+            int end = 0;
+
+            if (a > b)
+            {
+                start = b;
+                end = a;
+            }
+            else
+            {
+                start = a;
+                end = b;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                answer += i;
+            }
+
+
+            return answer;
+        }
+    }
+}
