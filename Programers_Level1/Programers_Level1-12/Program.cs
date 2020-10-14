161afbd6a1ca223ae45b93ee3649818246176388
coalstn789 <tyoyt7@gmail.com>
2020-10-14 18:01:12 +0900

    프로그래머스 - 문자열 다루기 기본

---
+++ Programers_Level1/Programers_Level1-12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"{solution("abcd")}");
+            Console.WriteLine($"{solution("34234")}");
+
+        }
+
+        public static bool solution(string s)
+        {
+            bool answer = true;
+            int result;
+
+            if (s.Length == 4 || s.Length == 6)
+            {
+                if (!int.TryParse(s, out result))
+                    answer = false;
+            }
+            else
+            {
+                return false;
+            }
+           
+            return answer;
+        }
+    }
+}
