cffc145e799218a8e9fec6a02285562705328484
coalstn789 <tyoyt7@gmail.com>
2020-10-14 21:31:26 +0900

    프로그래머스 - 소수찾기 (에라토스테네스의 체)

---
+++ Programers_Level1/Programers_Level1-13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_13
+{
+    class Program
+    {
+        static string[] strArray = { "Jane", "Kim" };
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(strArray)}");
+        }
+
+        public static string solution(string[] seoul)
+        {
+            string answer = "";
+            int index = seoul.ToList().FindIndex(x => x.Equals("Kim") == true);
+            answer = $"김서방은 {index}에 있다";
+            return answer;
+        }
+    }
+}
---
+++ Programers_Level1/Programers_Level1-14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_14
+{
+    class Program
+    {
+        static int value = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(value)}");
+        }
+
+        public static int solution(int n)
+        {
+            int answer = 0;
+            bool[] list = Enumerable.Repeat<bool>(false, n).ToArray();
+            list[0] = true;
+            for (int i = 2; i <= n; i++)
+            {
+                if (list[i -1] == true) continue;
+                for (int j = i + i; j <= n; j = j + i)
+                {
+                    list[j - 1] = true;
+                }
+            }
+            answer = list.Where(x => x == false).Count();
+            return answer;
+        }
+    }
+}
+
