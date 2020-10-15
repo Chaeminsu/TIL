e87016fd11c225800fbb20814bb7f4c77e3a253e
coalstn789 <tyoyt7@gmail.com>
2020-10-15 16:14:16 +0900

    프로그래머스 - 수박수박수

---
+++ Programers_Level1/Programers_Level1-15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(3)}");
+            Console.WriteLine($"{solution(4)}");
+        }
+        public static string solution(int n)
+        {
+            string answer = "";
+            var a = Enumerable.Range(1, n).Select(x => x % 2 == 0 ? "박" : "수");
+            answer = string.Join("", a.ToArray());
+            return answer;
+        }
+    }
+}
