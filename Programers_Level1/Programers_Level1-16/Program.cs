019320d1d5d5c73d9139992a7001c409ba891ca0
coalstn789 <tyoyt7@gmail.com>
2020-10-15 17:37:04 +0900

    프로그래머스 - 시저 암호

---
+++ Programers_Level1/Programers_Level1-16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution("1234")}");
+            Console.WriteLine($"{solution("-1234")}");
+        }
+        public static int solution(string s)
+        {
+            int answer = 0;
+            int result = 0;
+            int.TryParse(s, out result);
+            answer = result;
+
+            return answer;
+        }
+    }
+}
---
+++ Programers_Level1/Programers_Level1-17/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution("aYyZ", 25)}");
+
+        }
+        public static string solution(string s, int n)
+        {
+            string answer = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char value = s[i];
+                int shiftCount = n;
+                if (s[i] == ' ')
+                {
+                    answer += value;
+                    continue;
+                }
+                if ((char)(value + shiftCount) > 'Z' && value < 'a')
+                {
+                    shiftCount -= ('Z' - value);
+                    value += (char)('Z' - value);
+                }
+                if ((char)(value + shiftCount) > 'z')
+                {
+                    shiftCount -= ('z' - value);
+                    value += (char)('z' - value);
+                }
+
+                if (value == 'Z' || value == 'z')
+                    value = (char)(value - 26);
+
+                char a = (char)(value + shiftCount);
+                answer += a.ToString();
+            }
+            return answer;
+
+
+
+
+
+            return answer;
+        }
+    }
+}
