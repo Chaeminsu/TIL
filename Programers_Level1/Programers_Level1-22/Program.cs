efed75dbe56c2e67f1a4e532b5a8407162ca1557
coalstn789 <tyoyt7@gmail.com>
2020-10-17 15:01:58 +0900

    프로그래머스 - 정수 내림차순 으로 배치하기

---
+++ Programers_Level1/Programers_Level1-22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(118372)}");
+            string[] strArray = n.ToString().Select(x => x.ToString()).ToArray();
+            Array.Sort(strArray, new MyCompare());
+        }
+
+        public static long solution(long n)
+        {
+            long answer = 0;
+            string[] strArray2 = n.ToString().Select(x => x.ToString()).OrderBy(y => y).Reverse().ToArray();
+            answer = Int32.Parse(string.Join("", strArray2));
+            return answer;
+        }
+        public class MyCompare : IComparer<string>
+        {
+            public MyCompare()
+            {
+            }
+            public int Compare(string x, string y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+    }
+}
