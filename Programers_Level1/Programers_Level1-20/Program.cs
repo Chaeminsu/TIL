94f2f7c142afe3389f25a41c9174198336368287
coalstn789 <tyoyt7@gmail.com>
2020-10-17 14:29:23 +0900

    프로그래머스 - 자연수 뒤집어배열로만들기. 자리수 더하기

---
+++ Programers_Level1/Programers_Level1-20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(123)}");
+        }
+        public static int solution(int n)
+        {
+            int answer = 0;
+            string value = n.ToString();
+            int result;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                int.TryParse(value[i].ToString(), out result);
+                answer += result;
+            }
+         
+            return answer;
+        }
+    }
+}
---
+++ Programers_Level1/Programers_Level1-21/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in solution(12345))
+            {
+                Console.WriteLine(item);
+            }
+          
+
+        }
+        public static int[] solution(long n)
+        {
+            int[] answer = new int[] { };
+            answer = n.ToString().Select(x => int.Parse(x.ToString())).Reverse().ToArray();
+            return answer;
+        }
+    }
+}
