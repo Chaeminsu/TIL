88153a7aea24dee8754da01ff42b168907d4faa9
coalstn789 <tyoyt7@gmail.com>
2020-10-18 16:27:20 +0900

    프로그래머스 - 제일작은수 제거하기, 정수 제곱근 판별

---
+++ Programers_Level1/Programers_Level1-23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(121)}");
+        }
+        public static long solution(long n)
+        {
+            long answer = 0;
+            int result;
+
+            if (int.TryParse(Math.Sqrt(n).ToString(), out result))
+            {
+                answer = (long)Math.Pow((result + 1), 2);
+            }
+            else
+            {
+                answer = -1;
+            }
+               
+
+            return answer;
+        }
+    }
+}
---
+++ Programers_Level1/Programers_Level1-24/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_24
+{
+    class Program
+    {
+        static int[] arr = { 4,3,2,1 };
+
+        static void Main(string[] args)
+        {
+            //int[] answer = new int[] { };
+            //if (arr.Length <= 1)
+            //{
+            //    answer = new int[] { -1 };
+            //    return answer;
+            //}
+            //int temp = arr.Min();
+            //answer = arr.Select(x => x).Where(y => y != temp).ToArray();
+            //return answer;
+            foreach (var item in solution(arr))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static int[] solution(int[] arr)
+        {
+            List<int> b = new List<int>(arr);
+            List<int> c = new List<int>(arr);
+            b.Sort();
+            c.Remove(b[0]);
+            if (c.Count == 0)
+            {
+                c.Add(-1);
+            }
+            return c.ToArray();
+
+
+
+           
+        }
+       
+    }
+}
