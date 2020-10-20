cb25deb5f8ce59f656e2bb0caea392cc8cd46295
coalstn789 <tyoyt7@gmail.com>
2020-10-20 15:22:29 +0900

    프로그래머스 - 예산

---
+++ Programers_Level1/Programers_Level1-35/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers_Level1_35
+{
+    class Program
+    {
+        static int[] d = { 1,3,2,5,4 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{solution(d, 10)}");
+        }
+        public static int solution(int[] d, int budget)
+        {
+            int answer = 0;
+            Array.Sort(d);
+            foreach (var item in d)
+            {
+                if (item > budget)
+                {
+                    break;
+                }
+                budget -= item;
+                ++answer;
+            }
+            return answer;
+        }
+    }
+}
