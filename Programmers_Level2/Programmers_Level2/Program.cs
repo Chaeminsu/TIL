d91971e8f1efb23c73bf7c0ba38352c547daf54a
coalstn789 <tyoyt7@gmail.com>
2020-11-10 22:41:22 +0900

    프로그래머스 - 삼각 달팽이

---
+++ Programmers_Level2/Programmers_Level2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers_Level2
+{
+    class Program
+    {
+        static int n = 4;
+        static void Main(string[] args)
+        {
+            foreach (int item in solution(n))
+            {
+                Console.Write($"{item}");
+            }
+        }
+        public static int[] solution(int n)
+        {
+            List<int> answer = new List<int>();
+            int[,] arrays = new int[n,n];
+
+            int num = 1;
+            int x = -1, y = 0;
+            int sumNum = Enumerable.Range(1, n).Sum();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (i % 3 == 0)
+                        x++;
+                    else if (i % 3 == 1)
+                        y++;
+                    else if (i % 3 == 2)
+                    {
+                        x--;
+                        y--;
+                    }
+                    arrays[x, y] = num++;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arrays[i, j] == 0) break;
+                    answer.Add(arrays[i, j]);
+                }
+            }
+            
+
+
+
+
+            return answer.ToArray();
+        }
+    }
+}
+
